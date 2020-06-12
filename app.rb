15b1c2d7f9f3675a0609d6c347923431b2d58b64
paulasoncini <paulasoncini@hotmail.com>
2020-06-12 19:49:16 +0000

    Gitpod final project v1

--- app.rb
+++ app.rb
@@ -15,5 +15,123 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-# events_table = DB.from(:events)
-# rsvps_table = DB.from(:rsvps)
+locations_table = DB.from(:locations)
+responses_table = DB.from(:responses)
+users_table = DB.from(:users)
+
+# so it is included in every route (instead of running this code in the first line of each page)
+before do
+    # SELECT * FROM users WHERE id = session[:user_id]
+    @current_user = users_table.where(:id => session[:user_id]).to_a[0]
+    puts @current_user.inspect
+end
+
+
+# Home page (all events)
+get "/" do
+    view "homepage"
+end
+
+get "/locations" do
+    @locations = locations_table.all
+    view "locations"
+end
+
+# Show a single event
+get "/locations/:id" do
+    @users_table = users_table
+    # SELECT * FROM events WHERE id=:id
+    @location = locations_table.where(:id => params["id"]).to_a[0]
+    # SELECT * FROM rsvps WHERE event_id=:id
+    @responses = responses_table.where(:location_id => params["id"]).to_a
+    # SELECT COUNT(*) FROM rsvps WHERE event_id=:id AND going=1
+    @count = responses_table.where(:location_id => params["id"], :can_help => true).count
+    
+    results = Geocoder.search(@location[:address])
+    @lat_long = results.first.coordinates.join(",")
+
+    view "location"
+end
+
+# Form to create a new RSVP
+get "/locations/:id/responses/new" do
+    @location = locations_table.where(:id => params["id"]).to_a[0]
+    view "new_response"
+end
+
+# Receiving end of new RSVP form
+post "/locations/:id/responses/create" do
+    responses_table.insert(:location_id => params["id"],
+                       :can_help => params["can_help"],
+                       :user_id => @current_user[:id],
+                    #  :name => params["name"],
+                    #  :whatsapp_number => params["whatsapp"],
+                       :comments => params["comments"])
+    @location = locations_table.where(:id => params["id"]).to_a[0]
+    view "create_response"
+end
+
+# Form to create a new user
+get "/users/new" do
+    view "new_user"
+end
+
+# Receiving end of new user form
+post "/users/create" do
+    users_table.insert(:name => params["name"],
+                        :email => params["email"],
+                        :whatsapp_number => params["whatsapp_number"],
+                        :password => BCrypt::Password.create(params["password"]))
+    view "create_user"
+end
+
+# Form to login
+get "/logins/new" do
+    view "new_login"
+end
+
+# Receiving end of login form
+post "/logins/create" do
+    puts params
+    email_entered = params["email"]
+    password_entered = params["password"]
+    # SELECT * FROM users WHERE email = email_entered
+    user = users_table.where(:email => email_entered).to_a[0]
+    if user
+        puts user.inspect
+        #test password entered against the one in the users table
+        if BCrypt::Password.new(user[:password]) == password_entered
+            # 'session' is like 'cookies', but more secure (so you can't change it in the 'inspect' part of the page)
+            # it allows us to access the cookies in an encrypted way (Sinatra does this for us)
+            session[:user_id] = user[:id]
+            view "create_login"
+        else
+            view "create_login_failed"
+        end
+    else
+        view "create_login_failed"
+    end
+end
+
+# Logout
+get "/logout" do
+    session[:user_id] = nil
+    view "logout"
+end
+
+get "/send_sms" do
+    account_sid = ENV["TWILIO_ACCOUNT_SID"]
+    auth_token = ENV["TWILIO_AUTH_TOKEN"]
+
+    client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    from = "+12248084194"
+    to = @current_user[:whatsapp_number]
+    
+    client.messages.create(
+        from: from,
+        to: to,
+        body: "You have an appointment to help Paula move out"
+    )
+    view "send_sms"
+end
--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,40 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :locations do
   primary_key :id
   String :title
   String :description, text: true
-  String :date
+  String :date_and_time
   String :location
+  String :address
 end
-DB.create_table! :rsvps do
+DB.create_table! :responses do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
+  foreign_key :location_id
+  foreign_key :user_id
+  Boolean :can_help
+  String :comments, text: true
+end
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :whatsapp_number
+  String :password
 end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+locations_table = DB.from(:locations)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+locations_table.insert(title: "Clean out my fridge", 
+                    description: "I left a lot of food in there. Please help me throw away what went bad, donate what is still good (and feel free to keep whatever you want!)",
+                    date_and_time: "June 15 at 2pm",
+                    location: "my apartment",
+                    address: "1715 Chicago Ave, Evanston, IL 60201")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+locations_table.insert(title: "Organize my storage unit", 
+                    description: "I have hired movers to do the heavy lifting, but I need your help to make sure everything has been storaged properly.",
+                    date_and_time: "June 15 at 10am",
+                    location: "Public Storage Evanston",
+                    address: "2050 Green Bay Rd, Evanston, IL 60201")
